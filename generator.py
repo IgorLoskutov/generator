e9e98b8d4269bc6c71b6dfa82f99788d4739b179
IgorLoskutov <igor.loskutov@gefco.net>
2019-01-17 09:35:08 +0200

    final final

--- generator.py
+++ generator.py
@@ -21,16 +21,16 @@ class SqlYamler(object):
 			FOR EACH ROW EXECUTE PROCEDURE update_{table}();\n\n
 			'''
 	_one_to_many = '''
-			ALTER TABLE {child} ADD {parent}_ID INT NOT NULL,
+			\rALTER TABLE {child} ADD {parent}_ID INT NOT NULL,
 			ADD CONSTRAINT FK_{child}{parent}
 			FOREIGN KEY {parent}_ID REFERENCES {parent}({parent}_ID);
 			'''
 	_linking_table = '''
-			CREATE TABLE {table1}_{table2}(
-				{table1}_ID INT NOT NULL,
-				{table2}_ID INT NOT NULL,
-				PRIMARY KEY ({table1}_ID, {table2}_ID)
-				);
+			\rCREATE TABLE {table1}_{table2}(
+			{table1}_ID INT NOT NULL,
+			{table2}_ID INT NOT NULL,
+			PRIMARY KEY ({table1}_ID, {table2}_ID)
+			);
 			'''
 	_many_to_many = '''
 			ALTER TABLE {table1}_{table2},
@@ -44,6 +44,10 @@ class SqlYamler(object):
 	def __init__(self, yaml_file):
 		with open(yaml_file, 'r') as file:
 			self.schema = yaml.load(file)
+			self.many_to_many = dict()
+			self.one_to_many = dict()
+			self.many_to_one = dict()
+			self.__get_relations()
 	
 	def _fields(self, entity):
 		fields_list = []
@@ -57,60 +61,60 @@ class SqlYamler(object):
 			for entity in self.schema.keys():
 				output.write(self._create_query.format(table=entity, fields = self._fields(entity)))
 				output.write(self._trigger_query.format(table=entity))
+			output.write(self._relation_query())
 	
-	def update_relations(self):
-		many_to_many = dict()
-		one_to_many = dict()
-		many_to_one = dict()
-			
+	def __get_relations(self):
 		for entity in self.schema.keys():
 			if 'relations' in self.schema[entity]:
 				keens = self.schema[entity]['relations']
 				for keen in keens:
 					if keens[keen] == 'one':
 						if self.schema[keen]['relations'][entity] == 'many':
-							one_to_many[(keen, entity)] = 1
+							self.one_to_many[(keen, entity)] = 1
 							
 					if keens[keen] == 'many':
 						if self.schema[keen]['relations'][entity] == 'many':
-							if (entity, keen) not in many_to_many:
-								many_to_many[(keen, entity)] = 1
+							if (entity, keen) not in self.many_to_many:
+								self.many_to_many[(keen, entity)] = 1
 							else:
-								many_to_many[(entity, keen)] += 1
+								self.many_to_many[(entity, keen)] += 1
 						if self.schema[keen]['relations'][entity] == 'one':
-							many_to_one[(keen, entity)] = 1
+							self.many_to_one[(keen, entity)] = 1
 		
-		with open("./yml_schema.sql",'w') as output:
-			for relation in many_to_many:
-				if many_to_many[relation] != 2:
-					raise NotSupported
-				output.write(
-					__class__._linking_table.format(
-									table1=relation[0],
-									table2=relation[1]
-									)
-					 +
-					 __class__._many_to_many.format(
-									table1=relation[0],
-									table2=relation[1]
-									)
-					)						
+	def _relation_query(self):	
 		
-			for relation in one_to_many:
-				print(relation[::-1])
-				if many_to_one[relation[::-1]] != 1:
-					raise NotSupported
-				output.write(
-					 __class__._one_to_many.format(
-									child=relation[1],
-									parent=relation[0]
-									)
-					)						
-
+		relation_query  = ''
+		for relation in self.many_to_many:
+			if self.many_to_many[relation] != 2:
+				raise NotSupported
+			relation_query =''.join((
+				__class__._linking_table.format(
+								table1=relation[0],
+								table2=relation[1]
+								),
+				 
+				 __class__._many_to_many.format(
+								table1=relation[0],
+								table2=relation[1]
+								)
+				))						
+	
+		for relation in self.one_to_many:
+			if self.many_to_one[relation[::-1]] != 1:
+				raise NotSupported
+			relation_query = ''.join( (relation_query,
+				 __class__._one_to_many.format(
+								child=relation[1],
+								parent=relation[0]
+								)
+				))
+		return relation_query
+			
+	
 
 
 if __name__ == '__main__':
 
 	a = SqlYamler('./tables.yaml')
-	a.update_relations()
+	a.query()
 
